9caaa6fc63ea4dfbecd3a9f003b641f5b1d3728a
Noam Imstepf <noam@imstpef.net>
2024-12-03 16:18:14 +0100

    Ajout de l'herbe

--- main.py
+++ main.py
@@ -6,11 +6,29 @@ pygame.init()
 class Joueur(pygame.sprite.Sprite):
     def __init__(self):
        super().__init__() #Appel obligatoire
-       self.image = pygame.image.load("assets/mario.png").convert_alpha()
+       self.image = pygame.image.load("assets/knight.png").convert_alpha()
+       self.image = pygame.transform.scale_by(self.image, 2)
        self.rect = self.image.get_rect()
        self.rect.x = LARGEUR/2
        self.rect.y = HAUTEUR/2
        self.vitesse = 13
+
+class Plateforme(pygame.sprite.Sprite):
+    def __init__(self, image):
+        super().__init__()
+        self.image = pygame.image.load(image).convert_alpha()
+        self.image = pygame.transform.scale_by(self.image, 2)
+        self.rect = self.image.get_rect()
+        self.rect.x = LARGEUR/2
+        self.rect.y = HAUTEUR/2
+
+class Herbe(Plateforme):
+    def __init__(self):
+        image = "assets/herbe.png"
+        super().__init__(image)
+
+
+
 
 LARGEUR = 600
 HAUTEUR = 600
@@ -19,6 +37,8 @@ clock = pygame.time.Clock()
 joueur = Joueur()
 liste_des_sprites = pygame.sprite.LayeredUpdates()
 liste_des_sprites.add(joueur)
+herbe = Herbe()
+liste_des_sprites.add(herbe)
 running = True
 pygame.key.set_repeat(40, 30)
 
