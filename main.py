9fb62cff6a8828a0e2f44e35d682e3b512977083
Max <maximilianmax@outlook.de>
2024-12-17 16:14:09 +0100

    creation d'un constructeur pour grilles

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ pygame.init()
 liste_des_elements = []
 
 pesanteur_haut: int = 2
-pesanteur_bas: int = 6
+pesanteur_bas: int = 7
 
 class Joueur(pygame.sprite.Sprite):
     def __init__(self):
@@ -18,11 +18,11 @@ class Joueur(pygame.sprite.Sprite):
        self.image = pygame.transform.scale_by(self.image, 2)
        self.rect = self.image.get_rect()
        self.rect.x = LARGEUR/2
-       self.rect.y = HAUTEUR/2-200
-       self.vitesse = 13
+       self.rect.y = 100 #HAUTEUR/2
+       self.vitesse = 7
        self.saut = False
        self.est_dans_lair = False
-       self.hauteur_saut = 8
+       self.hauteur_saut = 10
        self.vitesse_de_saut = 0
 
     def actualiser(self):
@@ -46,38 +46,91 @@ class Plateforme(pygame.sprite.Sprite):
         liste_des_elements.append(self)
 
 class Herbe(Plateforme):
-    def __init__(self):
-        image = "assets/herbe.png"
+    def __init__(self, x, y):
+        image = "assets/blocs/herbe.png"
         super().__init__(image)
+        self.rect.x = x
+        self.rect.y = y
 
+class Terre(Plateforme):
+    def __init__(self, x, y):
+        image = "assets/blocs/terre.png"
+        super().__init__(image)
+        self.rect.x = x
+        self.rect.y = y
+
+class Interrogation(Plateforme):
+    def __init__(self, x, y):
+        image = "assets/blocs/interrogation.png"
+        super().__init__(image)
+        self.rect.x = x
+        self.rect.y = y
+
+
+class Grille():
+        def creer(self, listeDeGrille):
+            pos_y = 10*32
+            for ligne in listeDeGrille:
+                pos_x = 0
+                for element in ligne:
+                    if element == 1:
+                        liste_des_sprites.add(Herbe(pos_x, pos_y))
+                    if element == 2:
+                        liste_des_sprites.add(Terre(pos_x, pos_y))
+                    if element == 3:
+                        liste_des_sprites.add(Interrogation(pos_x, pos_y))
+                    pos_x += 32
+                pos_y += 32
+
+grille1 = [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+           [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+           [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+           [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+           [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2],
+           [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2],
+           [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+           [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]]
 
 fenetre = pygame.display.set_mode((LARGEUR, HAUTEUR))
 clock = pygame.time.Clock()
 joueur = Joueur()
 liste_des_sprites = pygame.sprite.LayeredUpdates()
 liste_des_sprites.add(joueur)
-herbe = Herbe()
-liste_des_sprites.add(herbe)
 
 running = True
-pygame.key.set_repeat(40, 30)
 
+droite_appuyé = False
+gauche_appuyé = False
+
+GrilleDeJeu = Grille()
+GrilleDeJeu.creer(grille1)
 
 while running:
    for event in pygame.event.get():
         if event.type == pygame.QUIT:
            running = False
+        if event.type == KEYUP:
+            if event.key == K_a:
+                gauche_appuyé = False
+            if event.key == K_d:
+                droite_appuyé = False
         if event.type == KEYDOWN:
             if event.key == K_a:
-                if joueur.rect.x > -10:
-                    joueur.rect.x -= joueur.vitesse
+                gauche_appuyé = True
             if event.key == K_d:
-                if joueur.rect.x < 570:
-                    joueur.rect.x += joueur.vitesse
+                droite_appuyé = True
             if event.key == K_SPACE and not joueur.saut and not joueur.est_dans_lair:
                 joueur.saut = True
                 joueur.est_dans_lair = True
                 joueur.vitesse_de_saut = joueur.hauteur_saut * 2
+
+   if gauche_appuyé:
+       if joueur.rect.x > -10:
+           joueur.rect.x -= joueur.vitesse
+   if droite_appuyé:
+       if joueur.rect.x < 570:
+           joueur.rect.x += joueur.vitesse
+
 
    joueur.actualiser()
 
@@ -88,7 +141,6 @@ while running:
                joueur.saut = False
                joueur.est_dans_lair = False
                joueur.vitesse_de_saut = 0
-
 
    fenetre.fill((255, 255, 255))
    liste_des_sprites.draw(fenetre)
